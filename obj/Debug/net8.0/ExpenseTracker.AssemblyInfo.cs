147052a5d844c2a74dc94f3c96571f89fcad7908
ZZM <zuezue3102000@gmail.com>
2024-12-18 16:42:54 +0630

    profile modify

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79adb780a1db1e30140a20873899be4eaba0b8bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a871b936a283608e7ca7116e49527698b806b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
